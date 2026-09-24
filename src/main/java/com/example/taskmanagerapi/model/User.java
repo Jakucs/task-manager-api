a4c5e8de7c60b84d8d3f5870fc49baf735993145
Jakucs Viktor <jakucs42@gmail.com>
2026-09-24 21:23:58 +0200

    add basic User class

---
+++ src/main/java/com/example/taskmanagerapi/model/User.java
@@ -0,0 +1,14 @@
+package com.example.taskmanagerapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String userName;
+    private String email;
+}
