56d820c9af6618a78a725fe3e7cacc46ca335e2d
Jakucs Viktor <jakucs42@gmail.com>
2026-09-25 17:01:33 +0200

    add UserService

--- src/main/java/com/example/taskmanagerapi/repository/UserRepository.java
+++ src/main/java/com/example/taskmanagerapi/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.example.taskmanagerapi.repository;
 
-public interface UserRepository {
+import com.example.taskmanagerapi.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
--- src/main/java/com/example/taskmanagerapi/service/UserService.java
+++ src/main/java/com/example/taskmanagerapi/service/UserService.java
@@ -1,4 +1,18 @@
 package com.example.taskmanagerapi.service;
 
+import com.example.taskmanagerapi.model.User;
+import com.example.taskmanagerapi.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
 }
